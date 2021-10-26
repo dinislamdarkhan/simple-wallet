dd11a27ccb51e154492994c0881b178f6d786d2e
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 00:12:31 +0600

    change request

--- src/wallet/presenter/data/request.go
+++ src/wallet/presenter/data/request.go
@@ -1,11 +1,9 @@
 package data
 
-import "time"
-
 type PostDoTransactionRequest struct {
-	UserID     string    `json:"user_id"`
-	Currency   string    `json:"currency"`
-	Amount     int       `json:"amount"`
-	Type       string    `json:"type"`
-	TimePlaced time.Time `json:"time_placed"`
+	UserID     string `json:"user_id"`
+	Currency   string `json:"currency"`
+	Amount     int    `json:"amount"`
+	Type       string `json:"type"`
+	TimePlaced string `json:"time_placed"`
 }
