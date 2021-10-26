ead4b5b294072987e258eadabf6504858cabd60e
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 02:28:37 +0600

    add request validation

--- src/wallet/presenter/http/transport.go
+++ src/wallet/presenter/http/transport.go
@@ -5,16 +5,19 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/dinislamdarkhan/simple-wallet/src/pkg/errors"
+
 	"github.com/dinislamdarkhan/simple-wallet/src/wallet/presenter"
 	"github.com/dinislamdarkhan/simple-wallet/src/wallet/presenter/data"
 	kithttp "github.com/go-kit/kit/transport/http"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
 func MakeHandler(service presenter.Service) http.Handler {
 	router := mux.NewRouter()
 	opts := []kithttp.ServerOption{
-		kithttp.ServerErrorEncoder(kithttp.DefaultErrorEncoder),
+		kithttp.ServerErrorEncoder(errors.EncodeErrorJSON),
 	}
 
 	router.Handle(
@@ -32,8 +35,15 @@ func MakeHandler(service presenter.Service) http.Handler {
 
 func decodeDoTransactionRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	var body data.PostDoTransactionRequest
+	validate := validator.New()
+
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		return nil, err
+		return nil, errors.DeserializeError
+	}
+
+	err := validate.Struct(body)
+	if err != nil {
+		return nil, errors.BadRequest.SetMessage(err.Error())
 	}
 
 	return body, nil
