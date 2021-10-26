476387b30d48a54d8ebea9d7d24210c44c95e4dc
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 01:17:51 +0600

    add cassandra table scheme

---
+++ schema/cassandra.sql
@@ -0,0 +1,12 @@
+drop table if exists wallet;
+create table if not exists wallet
+(
+    currency text,
+    user_id  text,
+    amount   decimal,
+    primary key (currency, user_id)
+)
+    with caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+     and compaction = {'max_threshold': '32', 'min_threshold': '4', 'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy'}
+     and compression = {'class': 'org.apache.cassandra.io.compress.LZ4Compressor', 'chunk_length_in_kb': '64'}
+;
