8cdd7c6217437330943d19fd71e6a25f7beefb16
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 01:17:25 +0600

    add cassandra repository

--- src/wallet/repository/cassandra/cassandra.go
+++ src/wallet/repository/cassandra/cassandra.go
@@ -1,6 +1,8 @@
 package cassandra
 
 import (
+	"context"
+
 	"github.com/dinislamdarkhan/simple-wallet/src/wallet/repository"
 	"github.com/gocql/gocql"
 )
@@ -13,4 +15,19 @@ func New(db *gocql.Session) repository.CassandraRepository {
 	return &cassandraRepository{
 		db: db,
 	}
+}
+
+func (c *cassandraRepository) UpdateWalletAmount(ctx context.Context, currency, userID string, amount float64) error {
+	if err := c.db.Query(`UPDATE wallet SET amount = ? WHERE currency = ? AND user_id = ?`, amount, currency, userID).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *cassandraRepository) GetWalletAmount(ctx context.Context, currency, userID string) (float64, error) {
+	var amount float64
+	if err := c.db.Query(`SELECT total_amount FROM wallet WHERE currency = ? AND user_id = ?`, currency, userID).Scan(&amount); err != nil {
+		return 0, err
+	}
+	return amount, nil
 }
--- src/wallet/repository/repository.go
+++ src/wallet/repository/repository.go
@@ -1,3 +1,10 @@
 package repository
 
-type CassandraRepository interface{}
+import (
+	"context"
+)
+
+type CassandraRepository interface {
+	UpdateWalletAmount(ctx context.Context, currency, userID string, amount float64) error
+	GetWalletAmount(ctx context.Context, currency, userID string) (float64, error)
+}
