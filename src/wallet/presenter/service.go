1d6b6bbc7eb54b6c973980e407ebd3c77f14381e
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 03:00:38 +0600

    add error handling in service

--- src/wallet/presenter/service.go
+++ src/wallet/presenter/service.go
@@ -3,6 +3,9 @@ package presenter
 import (
 	"context"
 
+	"github.com/dinislamdarkhan/simple-wallet/src/pkg/errors"
+	"github.com/dinislamdarkhan/simple-wallet/src/wallet/usecase"
+
 	"github.com/dinislamdarkhan/simple-wallet/src/wallet/presenter/data"
 
 	"github.com/dinislamdarkhan/simple-wallet/src/app/store"
@@ -20,6 +23,20 @@ func New(store store.RepositoryStore) Service {
 	return &service{store: store}
 }
 
-func (s *service) PostDoTransaction(ctx context.Context, req *data.PostDoTransactionRequest) (*data.PostDoTransactionResponse, error) {
-	return nil, nil
+func (s *service) PostDoTransaction(ctx context.Context, req *data.PostDoTransactionRequest) (response *data.PostDoTransactionResponse, err error) {
+	ch := make(chan error, 1)
+
+	go func() {
+		response, err = usecase.DoTransaction(ctx, &usecase.DoTransactionFacade{Store: s.store}, req)
+
+		ch <- err
+	}()
+
+	select {
+	case <-ctx.Done():
+		err = errors.NetworkTimeout
+	case err = <-ch:
+	}
+
+	return
 }
