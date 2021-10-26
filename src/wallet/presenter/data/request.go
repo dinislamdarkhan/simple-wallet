f63b6f46f9c2e5cb63d8caf6a423028d316cc4c9
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 03:01:05 +0600

    add DoTransaction usecase

--- src/wallet/presenter/data/request.go
+++ src/wallet/presenter/data/request.go
@@ -1,9 +1,9 @@
 package data
 
 type PostDoTransactionRequest struct {
-	UserID     string `json:"user_id" validate:"required"`
-	Currency   string `json:"currency" validate:"required,alpha"`
-	Amount     int    `json:"amount" validate:"required,gt=0"`
-	Type       string `json:"type" validate:"required,alpha"`
-	TimePlaced string `json:"time_placed" validate:"required"`
+	UserID     string  `json:"user_id" validate:"required"`
+	Currency   string  `json:"currency" validate:"required,alpha"`
+	Amount     float64 `json:"amount" validate:"required,gt=0"`
+	Type       string  `json:"type" validate:"required,alpha"`
+	TimePlaced string  `json:"time_placed" validate:"required"`
 }
--- src/wallet/usecase/do_transaction.go
+++ src/wallet/usecase/do_transaction.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"time"
 
+	"github.com/dinislamdarkhan/simple-wallet/src/pkg/errors"
+	"github.com/shopspring/decimal"
+
 	"github.com/dinislamdarkhan/simple-wallet/src/app/store"
 	"github.com/dinislamdarkhan/simple-wallet/src/wallet/presenter/data"
-	"github.com/shopspring/decimal"
+)
+
+const (
+	respMessage = "Successfully changed wallet amount"
 )
 
 type DoTransactionRepository interface {
 	GetWalletAmountCassandra(ctx context.Context, currency, userID string) (float64, error)
-	UpdateWalletAmountCassandra(ctx context.Context, currency, userID string, amount decimal.Decimal) error
+	UpdateWalletAmountCassandra(ctx context.Context, currency, userID string, amount float64, updatedTime time.Time) error
 }
 
 type DoTransactionFacade struct {
@@ -26,6 +32,27 @@ func (f *DoTransactionFacade) UpdateWalletAmountCassandra(ctx context.Context, c
 	return f.Store.WalletCassandra().UpdateWalletAmount(ctx, currency, userID, amount, updatedTime)
 }
 
-func DoTransaction(ctx context.Context, repo DoTransactionRepository, req *data.PostDoTransactionRequest) (*data.PostDoTransactionResponse, error) {
-	return nil, nil
+func DoTransaction(ctx context.Context, repo DoTransactionRepository, req *data.PostDoTransactionRequest) (resp *data.PostDoTransactionResponse, err error) {
+	amount, err := repo.GetWalletAmountCassandra(ctx, req.Currency, req.UserID)
+	if err != nil {
+		return nil, errors.CassandraReadError.SetMessage(err.Error())
+	}
+	currentAmount := decimal.NewFromFloat(amount)
+	reqAmount := decimal.NewFromFloat(req.Amount)
+
+	if req.Type == "deposit" {
+		currentAmount = currentAmount.Add(reqAmount)
+	} else if req.Type == "withdrawal" {
+		currentAmount = currentAmount.Sub(reqAmount)
+	}
+
+	utc := time.Now().UTC()
+	amount, _ = currentAmount.Float64()
+	err = repo.UpdateWalletAmountCassandra(ctx, req.Currency, req.UserID, amount, utc)
+	if err != nil {
+		return nil, errors.CassandraSaveError.SetMessage(err.Error())
+	}
+
+	resp.Message = respMessage
+	return resp, nil
 }
