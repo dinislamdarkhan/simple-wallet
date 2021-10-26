a170ed18ad7ce3e7b6fa36241d867cf4b1162162
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 01:18:17 +0600

    add cassandra repo in usecase

---
+++ src/wallet/usecase/do_transaction.go
@@ -0,0 +1,30 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/dinislamdarkhan/simple-wallet/src/app/store"
+	"github.com/dinislamdarkhan/simple-wallet/src/wallet/presenter/data"
+	"github.com/shopspring/decimal"
+)
+
+type DoTransactionRepository interface {
+	GetWalletAmountCassandra(ctx context.Context, currency, userID string) (float64, error)
+	UpdateWalletAmountCassandra(ctx context.Context, currency, userID string, amount decimal.Decimal) error
+}
+
+type DoTransactionFacade struct {
+	Store store.RepositoryStore
+}
+
+func (f *DoTransactionFacade) GetWalletAmountCassandra(ctx context.Context, currency, userID string) (float64, error) {
+	return f.Store.WalletCassandra().GetWalletAmount(ctx, currency, userID)
+}
+
+func (f *DoTransactionFacade) UpdateWalletAmountCassandra(ctx context.Context, currency, userID string, amount float64) error {
+	return f.Store.WalletCassandra().UpdateWalletAmount(ctx, currency, userID, amount)
+}
+
+func DoTransaction(ctx context.Context, repo DoTransactionRepository, req *data.PostDoTransactionRequest) (*data.PostDoTransactionResponse, error) {
+	return nil, nil
+}
