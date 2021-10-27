c76a2eca48dc295c499b09ff1cae443dd92cb306
Dinislam Darkhan <denis.darhan@gmail.com>
2021-10-27 16:16:26 +0600

    move req validation to usecase

--- src/wallet/presenter/http/transport.go
+++ src/wallet/presenter/http/transport.go
@@ -10,13 +10,7 @@ import (
 	"github.com/dinislamdarkhan/simple-wallet/src/wallet/presenter"
 	"github.com/dinislamdarkhan/simple-wallet/src/wallet/presenter/data"
 	kithttp "github.com/go-kit/kit/transport/http"
-	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
-)
-
-const (
-	currencyError      = "Incorrect currency, supports only USD,EUR"
-	operationTypeError = "Incorrect type, supports only deposit,withdrawal"
 )
 
 func MakeHandler(service presenter.Service) http.Handler {
@@ -40,23 +34,8 @@ func MakeHandler(service presenter.Service) http.Handler {
 
 func decodeDoTransactionRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	var body data.PostDoTransactionRequest
-	validate := validator.New()
-
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		return nil, errors.DeserializeError
-	}
-
-	err := validate.Struct(body)
-	if err != nil {
-		return nil, errors.BadRequest.SetMessage(err.Error())
-	}
-
-	if !data.CurrencyMap[body.Currency] {
-		return nil, errors.BadRequest.SetMessage(currencyError)
-	}
-
-	if !data.OperationTypeMap[body.Type] {
-		return nil, errors.BadRequest.SetMessage(operationTypeError)
+		return nil, errors.DeserializeError.SetMessage(err.Error())
 	}
 
 	return body, nil
